b10e02a41724d74c0d8b059a5dc83dea3f17b8cd
TugceGuven <tgcegvn@gmail.com>
2018-08-14 20:28:18 +0300

    Initial commit

---
+++ YMS5329_MVC_DataTransfer/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YMS5329_MVC_DataTransfer.Models;
+
+namespace YMS5329_MVC_DataTransfer.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+
+            List<Kategori> Kategoriler = new List<Kategori> {
+                new Kategori { ID=1,Ad="İçecekler",Aciklama="Süt,Kola,Ayran vb"},
+                new Kategori { ID=2,Ad="Deniz Mahsülleri",Aciklama="Hamsi,Somon Balığı,Levrek vb"},
+                new Kategori { ID=3,Ad="Tatlılar",Aciklama="Sütlaç,Baklava,Dondurma vb"}
+            };
+
+            ViewBag.KategoriListesi = Kategoriler;
+            ViewData["KategoriListesi"] = Kategoriler;
+            TempData["KategoriListesi"] = Kategoriler;
+            return View();
+        }
+
+        public ActionResult Category()
+        {
+
+            return View();
+
+        }
+
+    }
+}
---
+++ YMS5329_MVC_DataTransfer/Models/Kategori.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YMS5329_MVC_DataTransfer.Models
+{
+    public class Kategori
+    {
+        public int ID { get; set; }
+        public string Ad { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
